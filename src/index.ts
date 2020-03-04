1275c8e0866ecd78ce721ea7464d83214c438a7e
autumn crossan <the@autumn.zone>
2020-03-04 12:08:56 -0500

    renamed project to uni, set up basic routing

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
+import * as commander from 'commander'
+
 import { oneShot, watch } from './pulls'
 
-const isWatch = flags => flags.includes('--watch') || flags.includes('-w')
-const flags = process.argv.filter(i => /^\-/.test(i))
+commander
+  .command('pulls')
+  .option('-w, --watch', 'watch mode')
+  .description("get a list of your coworkers' open pull requests")
+  .action(async a => (a.watch ? await watch() : await oneShot()))
 
-if (!isWatch(flags)) {
-  oneShot()
-} else {
-  watch()
-}
+commander
+  .command('start <endDate>')
+  .description('start a sprint')
+  .action(endDate => console.log(endDate))
+
+commander
+  .command('todo <ticketNum>')
+  .description('add a to-do to your active sprint')
+  .action(ticketNum => console.log(ticketNum))
+
+commander.parse(process.argv)
